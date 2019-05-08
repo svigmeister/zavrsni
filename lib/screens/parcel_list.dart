773e6a03b44226d029a5414c4d28c05700eec5e8
Svigmeister <petar.svigir@fer.hr>
2019-05-08 18:52:30 +0200

    configuring basic options

---
+++ lib/screens/parcel_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+
+class ParcelList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ParcelListState();
+  }
+}
+
+class ParcelListState extends State<ParcelList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
